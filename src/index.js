22091cc601e6b04f54bf8c203ae40df70647554f
Nelson Pérez <bilthon@gmail.com>
2021-11-06 12:35:45 -0500

    feature: introduces rate limit to avoid the blockstream API to ban us

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ const bip39 = require('bip39')
 const bip32 = require('bip32')
 const bitcoin = require('bitcoinjs-lib')
 const axios = require('axios')
+const rateLimit = require('axios-rate-limit')
 
 // The desired gap, 20 is the recommended value.
 // But we can start with a lower value to try to find
 // the account quicker
 const GAP = 2
 
-const ACCOUNT_LIMIT = 2//10e3
+const ACCOUNT_LIMIT = 10e3
 
-const http = axios.create({
+const instance = axios.create({
   baseURL: 'https://blockstream.info/api'
 })
+const http = rateLimit(instance, { maxRPS: 1 })
 
 const main = async () => {
   const seedWords = process.env.SEED_WORDS
@@ -23,7 +25,6 @@ const main = async () => {
   console.log('seed: ', seed.toString('hex'))
   
   const root = bip32.fromSeed(seed)
-  console.log('root: ', root)
   
   const coin = root.derivePath('m/84\'/0\'')
   
@@ -35,12 +36,13 @@ const main = async () => {
       const address = bitcoin.payments.p2wpkh({
         pubkey: index.publicKey
       }).address
-      console.log(`m/84'/0'/${i}/0/${j} -> ${address}`)
       try {
         const resp = await http.get(`/address/${address}/txs`)
         if (resp.data.length > 0) {
           console.log('Account found! Index: ', i)
           return
+        } else {
+          console.log(`m/84'/0'/${i}/0/${j} -> ${address} - NOT`)
         }
       } catch(err) {
         console.error(err)
